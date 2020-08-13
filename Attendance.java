94b8bf99ec214fd1a662bcf10a945f90d996cba6
jg100 <jgiannelli13@gmail.com>
2020-08-13 14:00:19 -0700

    Create Attendance.java

---
+++ Attendance.java
@@ -0,0 +1,18 @@
+package CSC211Attendance;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+
+public class Attendance {
+    public static void main(String[] args) {
+        System.out.println("CSC211Attendance.Attendance: ");
+
+        File zoomChat  = new File("zoomchat");
+
+
+
+    }
+}
